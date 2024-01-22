aac2a7a8fe96cba9dcb5d28d755f2584307b2026
purrrplelipton <61193553+purrrplelipton@users.noreply.github.com>
2024-01-22 14:49:01 +0100

    attempt #3 of fixing deployment issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:3000/',
+				target: import.meta.env.API,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
